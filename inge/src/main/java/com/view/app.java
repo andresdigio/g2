1569afe0e7d92a7d3c39adc37dcd4b98ae6a5504
Martina Arco <marco@itba.edu.ar>
2017-12-07 19:08:59 -0300

    Me olvide unas cosas y .gitgnore

--- inge/src/main/java/com/view/app.java
+++ inge/src/main/java/com/view/app.java
@@ -12,13 +12,29 @@ public class app {
     private JButton button1;
     private JTextField username;
     private JTextField password;
+    private JTextArea signIn;
 
     public app() {
+        button1 = new JButton();
+        username = new JTextField();
+        password = new JTextField();
+
+        username.setToolTipText("Username");
+        password.setToolTipText("Password");
+
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 com.model.Control.login(username.getText(), password.getText());
             }
         });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("app");
+        frame.setContentPane(new app().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
