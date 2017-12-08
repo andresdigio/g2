60c325f8e57b8db193044054cf994444843eaf21
Unknown <jormachea@itba.edu.ar>
2017-12-08 11:45:38 -0300

    SingUpCompany

--- inge/src/main/java/com/view/SignUpCompany.java
+++ inge/src/main/java/com/view/SignUpCompany.java
@@ -1,7 +1,16 @@
 package com.view;
 
+import javax.swing.*;
+
 /**
  * Created by martina on 12/7/17.
  */
 public class SignUpCompany {
+    private JComboBox zone;
+    private JButton save;
+    private JButton cancel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JPanel picture;
+    private JPanel companySingUp;
 }
