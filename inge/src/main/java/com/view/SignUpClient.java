bf12944b83ac2a0db8cabdf647948855be9b0243
Unknown <jormachea@itba.edu.ar>
2017-12-08 10:50:26 -0300

    SingUp

--- inge/src/main/java/com/view/SignUpClient.java
+++ inge/src/main/java/com/view/SignUpClient.java
@@ -1,7 +1,23 @@
 package com.view;
 
+import javax.swing.*;
+
 /**
  * Created by martina on 12/7/17.
  */
 public class SignUpClient {
+    private JPanel singUp;
+    private JTextField firstName;
+    private JTextField lastName;
+    private JPasswordField passwordCreation;
+    private JPasswordField passwordConfirmation;
+    private JTextField address;
+    private JTextField zipcode;
+    private JTextField mobileNumber;
+    private JTextField workNumber;
+    private JComboBox city;
+    private JComboBox province;
+    private JComboBox country;
+    private JButton btSingUp;
+    private JButton btCancel;
 }
