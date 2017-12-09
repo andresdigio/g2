bf260d5942b95928b0e5fe0dd95e7a825e2bf1a8
Unknown <jormachea@itba.edu.ar>
2017-12-08 21:22:04 -0300

    CompanyRemake

--- inge/src/main/java/com/view/SignUpCompany.java
+++ inge/src/main/java/com/view/SignUpCompany.java
@@ -2,15 +2,21 @@ package com.view;
 
 import javax.swing.*;
 
-/**
- * Created by martina on 12/7/17.
- */
 public class SignUpCompany {
-    private JComboBox zone;
-    private JButton save;
-    private JButton cancel;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JPanel picture;
-    private JPanel companySingUp;
+    private JPanel panel1;
+    private JComboBox country;
+    private JComboBox province;
+    private JTextField address;
+    private JTextField zip;
+    private JTextField characteristics;
+    private JTextField phone;
+    private JComboBox comboBox1;
+    private JTextField incoterms;
+    private JTextField pkgsize;
+    private JTextField transportType;
+    private JTextField ContainerType;
+    private JTextField email;
+    private JPasswordField password;
+    private JTextField name;
+    private JComboBox pkgType;
 }
