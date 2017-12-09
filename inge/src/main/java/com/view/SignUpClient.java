0d45b087177de53060e24f65ddf41c08490a81c3
Martina Arco <marco@itba.edu.ar>
2017-12-09 15:44:40 -0300

    Se puede abrir signUpClient

--- inge/src/main/java/com/view/SignUpClient.java
+++ inge/src/main/java/com/view/SignUpClient.java
@@ -10,7 +10,7 @@ import java.awt.event.ActionListener;
  * Created by martina on 12/7/17.
  */
 public class SignUpClient {
-    JFrame frame;
+    private JFrame frame;
     public JPanel singUp;
     private JTextField name;
     private JTextField username;
@@ -18,23 +18,51 @@ public class SignUpClient {
     private JPasswordField passwordConfirmation;
     private JTextField address;
     private JTextField zipcode;
-    private JTextField mobileNumber;
-    private JTextField workNumber;
+    private JTextField telephoneNumber;
     private JComboBox city;
     private JComboBox province;
     private JComboBox country;
     private JButton btSingUp;
     private JButton btCancel;
+    private JTextField email;
 
-    public SignUpClient() {
+    public SignUpClient(JFrame frame) {
+
+        this.frame = frame;
 
         //TODO necesito inputs de los datos que estan en el comentario de la linea 35 y pasarlos asi como parametros.
         btSingUp.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //Control.signUpClient(username, pass, email, country, province, department, address, zip, tel_number, name);
+                System.out.println(String.valueOf(passwordConfirmation.getPassword()));
+                System.out.println(String.valueOf(passwordCreation.getPassword()));
+//                if(String.valueOf(passwordConfirmation.getPassword()).equals(String.valueOf(passwordCreation.getPassword())))
+//                    Control.signUpClient(username.getText(), String.valueOf(passwordCreation.getPassword()), email.getText(), country.getSelectedItem().toString(), province.getSelectedItem().toString(), city.getSelectedItem().toString(), address.getText(), zipcode.getText(), telephoneNumber.getText(), name.getText());
+//                else {
+//                    JOptionPane.showMessageDialog(null, "Confirmation password must match password");
+//                }
+            }
+        });
+
+        btCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("njdkjdal");
+                JFrame aux = new JFrame("app");
+                aux.setContentPane(new app(aux).panel1);
+                aux.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+                aux.pack();
+                frame.setVisible(false);
+                aux.setVisible(true);
             }
         });
     }
 
+//    public static void main(String[] args) {
+//        JFrame frame = new JFrame("SignUpClient");
+//        frame.setContentPane(new SignUpClient().singUp);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setVisible(true);
+//    }
 }
--- inge/src/main/java/com/view/app.java
+++ inge/src/main/java/com/view/app.java
@@ -8,17 +8,20 @@ import java.awt.event.ActionListener;
  * Created by Andres on 03/12/2017.
  */
 public class app extends JFrame{
-    private JPanel panel1;
+    public static JFrame appFrame;
+    public JPanel panel1;
     private JButton button1;
     private JTextField username;
     private JTextField password;
     private JTextArea signIn;
     private JButton signUpButton;
 
-    public app() {
+    public app(JFrame frame) {
         button1 = new JButton();
         username = new JTextField();
         password = new JTextField();
+
+        appFrame = frame;
 
         username.setToolTipText("Username");
         password.setToolTipText("Password");
@@ -32,20 +35,21 @@ public class app extends JFrame{
         signUpButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFrame frame = new JFrame("SignUpClient");
-                frame.setContentPane(new SignUpClient().singUp);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.pack();
-                frame.setVisible(true);
+                JFrame aux = new JFrame("SignUpClient");
+                aux.setContentPane(new SignUpClient(aux).singUp);
+                aux.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+                aux.pack();
+                appFrame.setVisible(false);
+                aux.setVisible(true);
             }
         });
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("app");
-        frame.setContentPane(new app().panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        appFrame = new JFrame("app");
+        appFrame.setContentPane(new app(appFrame).panel1);
+        appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        appFrame.pack();
+        appFrame.setVisible(true);
     }
 }
